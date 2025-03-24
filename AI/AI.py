3bc5bdc7154dbe0885c9b3db7fc948d2fc42d5ef
Penguin2311 <111010740+Penguin2311@users.noreply.github.com>
2025-03-24 19:02:02 +0000

    Add Image Support

--- AI/AI.py
+++ AI/AI.py
@@ -3,17 +3,17 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from pydantic import BaseModel
+import PIL
 
 # Load environment variables
 load_dotenv()
 # Constants
 
-class AI:
-    client = genai.Client(api_key=os.getenv("API_KEY"))
+client = genai.Client(api_key=os.getenv("API_KEY"))
 
-    @classmethod
-    def get_response(cls, chat_history, user_profile):
-        system_instruction = """
+
+def get_response(chat_history, user_profile):
+    system_instruction = """
 You are EcoGenie, a friendly and helpful AI assistant passionate about sustainability. 
 Your purpose is to provide information, tips, and resources to help users live more eco-consciously. 
 
@@ -26,44 +26,45 @@ start your response with {{new_profile: "updated profile text"}}, followed by a 
 
 If a user asks a question unrelated to sustainability, politely inform them that you are focused on helping people live more sustainably and cannot answer their question.
 """     
-        response = cls.client.models.generate_content(
-            model = "gemini-2.0-flash",
-            contents = [types.Content(role=content.get("role"), parts=[types.Part.from_text(text=content.get("parts"))]) for content in chat_history],
-                config=types.GenerateContentConfig(
-                    system_instruction=system_instruction.format(user_profile=user_profile),
-            ),
-        )
-        return response.text
-    
-    @classmethod
-    def make_profile(cls, info):
-        system_instruction = """
+    contents = get_content_from_json_chat(chat_history)
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents = contents,
+        config=types.GenerateContentConfig(
+                system_instruction=system_instruction.format(user_profile=user_profile),
+        ),
+    )
+    return response.text
+
+
+def make_profile(info):
+    system_instruction = """
 You are EcoGenie, a friendly and helpful AI assistant passionate about sustainability. 
 Your purpose is to provide information, tips, and resources to help users live more eco-consciously. 
 For this task, you will be given a list of questions and a user's response to them and asked to create a profile based on the details provided.
 Respond with just a short and concise profile summary that captures the user's key characteristics, lifestyle, and sustainability habits from their responses.
 Don't add anything else to the response.
 """
-        response = cls.client.models.generate_content(
-            model = "gemini-2.0-flash-lite",
-            contents = [
-                types.Content(role="user", parts = [types.Part.from_text(text=info)])
-            ],
-                config=types.GenerateContentConfig(
-                    system_instruction=system_instruction,
-            ),
-        )
-        return response.text
-    
-    @classmethod
-    def summarize_chat(cls, chat_history):
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash-lite",
+        contents = [
+            types.Content(role="user", parts = [types.Part.from_text(text=info)])
+        ],
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction,
+        ),
+    )
+    return response.text
 
-        # output schema
-        class Content(BaseModel):
-            role: str
-            parts: str
-        
-        system_instruction = """
+
+def summarize_chat(chat_history):
+
+    # output schema
+    class Content(BaseModel):
+        role: str
+        parts: str
+    
+    system_instruction = """
 You will be given a json string containing a conversation between a user and a model.
 The conversation will be between two roles: "user" and "model".
 You should summarize the conversation by keeping the role and summarizing the parts of the conversation keeping only the most important information.
@@ -73,13 +74,33 @@ If the "Conversation summary" is too long, you can further summarize the older i
 
 Return response in json format [{}]
 """
-        response = cls.client.models.generate_content(
-            model = "gemini-2.0-flash",
-            contents = [types.Content(role="user", parts=[types.Part.from_text(text=str(chat_history))])],
-            config=types.GenerateContentConfig(
-                    system_instruction=system_instruction,
-                    response_mime_type= 'application/json',
-                    response_schema= list[Content],
-            ),
-        )
-        return response.text
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents = [types.Content(role="user", parts=[types.Part.from_text(text=str(chat_history))])],
+        config=types.GenerateContentConfig(
+                system_instruction=system_instruction,
+                response_mime_type= 'application/json',
+                response_schema= list[Content],
+        ),
+    )
+    return response.text
+
+
+def get_content_from_json_chat(json_chat):
+    contents = []
+    for content in json_chat:
+        role = content["role"]
+        parts = content["parts"]
+
+        if isinstance(parts, str):
+            contents.append(types.Content(role=role, parts=[types.Part.from_text(text=parts)]))
+
+        elif isinstance(parts, bytes):
+            print(parts)
+            contents.append(types.Content(role=role, parts=[types.Part.from_bytes(data=parts, mime_type="image/png")]))
+            
+        elif isinstance(parts, list):
+            contents.append(types.Content(role=role, parts=[types.Part.from_text(text=part) if isinstance(part, str) 
+                                                            else types.Part.from_bytes(data=part, mime_type="image/png") if isinstance(part, bytes) 
+                                                            else types.Part.from_text(text="There was some error.") for part in parts]))
+    return contents
